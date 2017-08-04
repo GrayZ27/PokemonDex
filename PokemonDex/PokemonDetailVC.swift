32a4f1dccf0b4eca01bbca327df3e84ba8c675b5
GrayZ27 <i520you.chen@qq.com>
2017-08-04 16:27:32 -0500

    Added IBOutlet for detail page and a back button function

--- PokemonDex/PokemonDetailVC.swift
+++ PokemonDex/PokemonDetailVC.swift
@@ -13,10 +13,27 @@ class PokemonDetailVC: UIViewController {
     
     var pokemon: Pokemon!
     
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var pokedexLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+    @IBAction func backBtnPressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
 
 }
